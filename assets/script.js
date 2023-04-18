f857b86340ea7787ec41ca53d4b79120e99d87b0
Camille MacLaren <camillemaclaren@gmail.com>
2023-04-18 02:54:40 -0500

    Added Javascript that shows topics in the console

---
+++ assets/script.js
@@ -0,0 +1,18 @@
+var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
+
+for (var i = 0; i < topics.length; i++)
+{
+    console.log(topics[i]);
+}
+/* //comment out section
+if (topic === 'HTML')
+{    console.log("let's study HTML!");  }
+else if (topic == 'CSS')
+{    console.log("Let's study CSS!");   }
+else if (topic === 'Git') 
+{    console.log("Let's study Git!");   }
+else if (topic === 'JavaScript') 
+{    console.log("Let's study JavaScript!");    }
+ else 
+{  console.log('Please try again!');    }
+// */ //end of comment out section
